d35ce4b90c29600dd6ec02e906590570168ef2bf
AxelC-Reyes <112121914+AxelC-Reyes@users.noreply.github.com>
2025-07-26 18:17:09 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
-
-// Importa los servicios necesarios para inicialización global
-import { AuthProvider } from './context/AuthContext'; // Opcional si usas Context API
-import './services/axiosConfig'; // Si usas Axios para llamadas API
+import { AuthProvider } from './context/AuthContext';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    {/* Si implementas Context API para autenticación */}
-    {/* <AuthProvider> */}
+    <AuthProvider>
       <App />
-    {/* </AuthProvider> */}
+    </AuthProvider>
   </StrictMode>
 );
